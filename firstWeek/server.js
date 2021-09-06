0c290465ec682eb0cf5c86e6df190d9905c5b7e2
hheatgithub <tohongho@gmail.com>
2021-09-06 17:45:36 -0500

    format files

--- firstWeek/server.js
+++ firstWeek/server.js
@@ -1,22 +1,18 @@
-const http = require('http')
+const http = require('http');
 
 http.createServer((request, response) => {
-
-    let body = []
-    request.on('error', (err) => {
-        console.error(err)
+  const body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  })
+    .on('data', (chunk) => {
+      body.push(chunk.toString());
     })
-        .on('data', (chunk) => {
-            body.push(chunk.toString())
-        })
-        .on('end', () => {
-            console.log("end from client:", body)
-            //body = Buffer.concat(body).toString();
+    .on('end', () => {
+      console.log('end from client:', body);
+      // body = Buffer.concat(body).toString();
 
-            response.writeHead(200, { 'Content-Type': 'text/html' })
-            response.end('Hello from server')
-        })
-
-
-
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end('Hello from server');
+    });
 }).listen(8088, '127.0.0.1');
